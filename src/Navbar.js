49f3989ab76d384dd06eb5befb64fa740e0a06ee
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-15 18:48:11 +0400

    init: initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <h1>Hi Taiwo Ogunleye!</h1>
+            <h2>I am Navbar</h2>
+        </>
+    )
+}
+
+
+export default Navbar
