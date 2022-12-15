f454352bc9f6c8f64d47810cd3d6cd719a1089a9
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-15 19:25:47 +0400

    feat: navbar markup update

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <>
-            <h1>Hi Taiwo Ogunleye!</h1>
+        <nav className='navbar'>
             <h2>I am Navbar</h2>
-        </>
+            <h1>The Main Heading</h1>
+            <div className='links'></div>
+            <p><a href='/'>Home</a></p>
+            <p><a href='/create'>New Page</a></p>
+        </nav >
     )
 }
 
