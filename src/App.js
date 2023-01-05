49d08d0f7ab0e995a9981199a25271d79d860c93
IamNatetrizton <taiwotrizton@gmail.com>
2023-01-05 12:11:30 +0100

    lesson 19

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className="App">
             <Navbar />
-            <header className="App-header">
+            <header className="content">
                 <Home />
 
                 <p>Good Luck Taiwo Ogunleye!</p>
